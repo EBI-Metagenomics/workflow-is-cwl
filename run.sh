ad304952a17318d4a21e259bd94abb59f87f1abe
mscheremetjew <maxim@ebi.ac.uk>
2018-03-13 14:35:36 +0000

    Renamed CWL file for I5.

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 cd tools/InterProScan/
-cwl-runner InterProScan-5.28-67.0.cwl InterProScan-apps.yaml --proteinFile test_single_protein.fasta
+cwl-runner InterProScan-5.cwl InterProScan-apps.yaml --proteinFile test_single_protein.fasta
