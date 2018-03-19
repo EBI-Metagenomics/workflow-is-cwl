a71f19453e6be07a83eec4d2336a8056cfa099fd
Maxim <maxim.scheremetjew@gmail.com>
2018-03-19 23:08:42 +0000

    Update install-cwl-runner.sh

--- install-cwl-runner.sh
+++ install-cwl-runner.sh
@@ -1,1 +1,15 @@
+#!/usr/bin/env bash
 
+sudo apt-get update
+wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
+bash miniconda.sh -b -p $HOME/miniconda
+export PATH="$HOME/miniconda/bin:$PATH"
+hash -r
+conda config --set always_yes yes --set changeps1 no
+conda update -q conda
+# Useful for debugging any issues with conda
+conda info -a
+
+conda create -q -n cwl-environment python=$TRAVIS_PYTHON_VERSION
+source activate cwl-environment
+pip install -r requirements.txt
