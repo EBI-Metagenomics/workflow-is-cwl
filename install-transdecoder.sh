f716e26cc2cc8d77971818787fc3a73b9eb39a12
Maxim <maxim.scheremetjew@gmail.com>
2018-03-19 23:11:59 +0000

    Update install-transdecoder.sh

--- install-transdecoder.sh
+++ install-transdecoder.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 wget https://github.com/TransDecoder/TransDecoder/archive/TransDecoder-v5.0.2.tar.gz
 tar -xvzf TransDecoder-v5.0.2.tar.gz -C $HOME
 export PATH="$HOME/TransDecoder-TransDecoder-v5.0.2:$PATH"
