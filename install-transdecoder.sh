6063dc9871ead565e8ae0e5cfe0038d116804d56
Maxim <maxim.scheremetjew@gmail.com>
2018-03-19 23:11:21 +0000

    Create install-transdecoder.sh

---
+++ install-transdecoder.sh
@@ -0,0 +1,3 @@
+wget https://github.com/TransDecoder/TransDecoder/archive/TransDecoder-v5.0.2.tar.gz
+tar -xvzf TransDecoder-v5.0.2.tar.gz -C $HOME
+export PATH="$HOME/TransDecoder-TransDecoder-v5.0.2:$PATH"
