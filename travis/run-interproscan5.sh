b4fbb2cf5150d206fb89ebdde9f6052b9af09e72
mscheremetjew <maxim@ebi.ac.uk>
2018-03-20 13:41:55 +0000

    Added InterProScan folder to the PATH.

--- travis/run-interproscan5.sh
+++ travis/run-interproscan5.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 
+export PATH="tools/InterProScan/:$PATH"
+
 cd tools/InterProScan/
 cwl-runner InterProScan-v5.cwl InterProScan-apps.yaml --proteinFile test_single_protein.fasta
