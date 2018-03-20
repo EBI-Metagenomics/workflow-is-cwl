e145c8c4fe8530edfe42eac289a8860a6c716d5c
mscheremetjew <maxim@ebi.ac.uk>
2018-03-20 15:16:39 +0000

    Reactivated bash call again.

--- travis/run-interproscan5.sh
+++ travis/run-interproscan5.sh
@@ -8,6 +8,7 @@ mkdir $HOME/InterProScan
 cp interproscan.sh $HOME/InterProScan
 export PATH="$HOME/InterProScan:$PATH"
 echo $PATH
+bash interproscan.sh
 
 cd tools/InterProScan/
 cwl-runner InterProScan-v5.cwl InterProScan-apps.yaml --proteinFile test_single_protein.fasta
