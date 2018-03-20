296dcc24a4bc914eb13235480be645d636393f3d
mscheremetjew <maxim@ebi.ac.uk>
2018-03-20 12:18:41 +0000

    Specified python version.

--- travis/install-cwl-runner.sh
+++ travis/install-cwl-runner.sh
@@ -10,6 +10,6 @@ conda update -q conda
 # Useful for debugging any issues with conda
 conda info -a
 
-conda create -q -n cwl-environment python=$TRAVIS_PYTHON_VERSION
+conda create -q -n cwl-environment python=3.6.4
 source activate cwl-environment
 pip install -r travis/requirements.txt
