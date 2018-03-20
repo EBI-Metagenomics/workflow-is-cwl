4b9167424ce1fe3997b850e64735cb40b14fafca
mscheremetjew <maxim@ebi.ac.uk>
2018-03-20 13:42:38 +0000

    Added miniconda to the PATH. Added env activation command.

--- travis/run-transdecoder.sh
+++ travis/run-transdecoder.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
 export PATH="$HOME/TransDecoder-TransDecoder-v5.0.2:$PATH"
+export PATH="$HOME/miniconda/bin:$PATH"
 echo $PATH
+source activate cwl-environment
 
 cd tools/TransDecoder
 cwl-runner TransDecoder.LongOrfs-v5.cwl TransDecoder-v5-genetic_codes.yaml --transcriptsFile test_single_protein.fasta
