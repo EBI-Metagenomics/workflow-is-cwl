1a8bebd24612bf75db0a62827c7b684f846b6237
mscheremetjew <maxim@ebi.ac.uk>
2018-03-20 16:48:41 +0000

    Expanded PATH and sourced cwl-runner env.

--- travis/run-interproscan5.sh
+++ travis/run-interproscan5.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
 export PATH="tools/InterProScan:$PATH"
+export PATH="$HOME/miniconda/bin:$PATH"
 echo $PATH
 bash interproscan.sh
+source activate cwl-environment
 
 cwl-runner InterProScan-v5.cwl InterProScan-apps.yaml --proteinFile test_single_protein.fasta
