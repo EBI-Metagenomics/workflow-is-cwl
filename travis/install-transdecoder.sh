509d0d8b48c8316a9311cd5600a740c26c1adfee
mscheremetjew <maxim@ebi.ac.uk>
2018-03-20 12:52:22 +0000

    Changed transdecoder run script.

--- travis/install-transdecoder.sh
+++ travis/install-transdecoder.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
+./install-cwl-runner.sh
 wget https://github.com/TransDecoder/TransDecoder/archive/TransDecoder-v5.0.2.tar.gz
 tar -xvzf TransDecoder-v5.0.2.tar.gz -C $HOME
--- travis/run-transdecoder.sh
+++ travis/run-transdecoder.sh
@@ -2,4 +2,6 @@
 
 export PATH="$HOME/TransDecoder-TransDecoder-v5.0.2:$PATH"
 echo $PATH
-TransDecoder.Predict -h
+
+cd tools/TransDecoder
+cwl-runner TransDecoder.LongOrfs-v5.cwl TransDecoder-v5-genetic_codes.yaml --transcriptsFile test_single_protein.fasta
