569261cc7430f61fcf4f9da48bb1d1d5f5f55a3c
mscheremetjew <maxim@ebi.ac.uk>
2018-03-20 13:48:17 +0000

    Added echo.

--- travis/run-interproscan5.sh
+++ travis/run-interproscan5.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-export PATH="tools/InterProScan/:$PATH"
+export PATH="tools/InterProScan:$PATH"
+echo $PATH
 
 cd tools/InterProScan/
 cwl-runner InterProScan-v5.cwl InterProScan-apps.yaml --proteinFile test_single_protein.fasta
