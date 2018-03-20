9d77d3b9e5cb160ca8690c74ea2c30e7c15c1778
mscheremetjew <maxim@ebi.ac.uk>
2018-03-20 14:36:01 +0000

    Added proper test file.

--- travis/run-transdecoder.sh
+++ travis/run-transdecoder.sh
@@ -6,4 +6,4 @@ echo $PATH
 source activate cwl-environment
 
 cd tools/TransDecoder
-cwl-runner TransDecoder.LongOrfs-v5.cwl TransDecoder-v5-genetic_codes.yaml --transcriptsFile test_single_protein.fasta
+cwl-runner TransDecoder.LongOrfs-v5.cwl TransDecoder-v5-genetic_codes.yaml --transcriptsFile test_transcripts.fasta
