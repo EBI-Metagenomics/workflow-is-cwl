3c87c2f90ef772b1d1e196b250088856c751c110
mscheremetjew <maxim@ebi.ac.uk>
2018-03-20 12:36:49 +0000

    moved export command.

--- travis/install-transdecoder.sh
+++ travis/install-transdecoder.sh
@@ -2,4 +2,3 @@
 
 wget https://github.com/TransDecoder/TransDecoder/archive/TransDecoder-v5.0.2.tar.gz
 tar -xvzf TransDecoder-v5.0.2.tar.gz -C $HOME
-export PATH="$HOME/TransDecoder-TransDecoder-v5.0.2:$PATH"
--- travis/run-transdecoder.sh
+++ travis/run-transdecoder.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
+export PATH="$HOME/TransDecoder-TransDecoder-v5.0.2:$PATH"
 TransDecoder.Predict -h
