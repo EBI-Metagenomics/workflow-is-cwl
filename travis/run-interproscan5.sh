ca980e3c29454930268bd9c57f69ed17c215fa67
mscheremetjew <maxim@ebi.ac.uk>
2018-03-20 17:04:13 +0000

    Added cd line.

--- travis/run-interproscan5.sh
+++ travis/run-interproscan5.sh
@@ -6,4 +6,5 @@ echo $PATH
 bash interproscan.sh
 source activate cwl-environment
 
+cd tools/InterProScan
 cwl-runner InterProScan-v5.cwl InterProScan-apps.yaml --proteinFile test_single_protein.fasta
