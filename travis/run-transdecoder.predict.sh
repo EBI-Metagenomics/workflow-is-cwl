ee9c43af42407bfeef976f8b4dda4d7178a99d16
mscheremetjew <maxim@ebi.ac.uk>
2018-04-03 15:14:57 +0100

    Added transdecoder predict test to travis.

---
+++ travis/run-transdecoder.predict.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+export PATH="$HOME/TransDecoder-TransDecoder-v5.0.2:$PATH"
+export PATH="$HOME/miniconda/bin:$PATH"
+echo $PATH
+source activate cwl-environment
+
+cd tools/TransDecoder
+cwl-runner TransDecoder.Predict-v5.cwl TransDecoder-v5-genetic_codes.yaml --transcriptsFile test-input/test_transcripts.fasta
