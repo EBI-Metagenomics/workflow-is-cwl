a6a389b5d9569134a9677ffef2f8fda83c26e8b7
mscheremetjew <maxim@ebi.ac.uk>
2018-04-05 11:26:10 +0100

    Applied fix to diamond install.

--- travis/install-diamond.sh
+++ travis/install-diamond.sh
@@ -2,4 +2,4 @@
 
 travis/install-cwl-runner.sh
 wget http://github.com/bbuchfink/diamond/releases/download/v0.9.18/diamond-linux64.tar.gz
-tar -xvzf diamond-linux64.tar.gz
+tar -xvzf diamond-linux64.tar.gz -C $HOME
