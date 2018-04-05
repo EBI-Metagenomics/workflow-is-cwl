c4de35450294411c57458374a9907fc8e122551f
mscheremetjew <maxim@ebi.ac.uk>
2018-04-05 11:39:43 +0100

    Added write permission.

--- travis/install-interproscan5.sh
+++ travis/install-interproscan5.sh
@@ -4,3 +4,4 @@ travis/install-cwl-runner.sh
 mkdir $HOME/interproscan5
 cd $HOME/interproscan5
 wget https://raw.githubusercontent.com/mscheremetjew/workflow-is-cwl/master/tools/InterProScan/interproscan.sh
+chmod +x interproscan.sh
