ff80a076ad4ee464eaf3618bd06501d3a1cc2d1a
mscheremetjew <maxim@ebi.ac.uk>
2018-04-17 16:28:46 +0100

    Removed double command call.

--- travis/install-phmmer.sh
+++ travis/install-phmmer.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 travis/install-cwl-runner.sh
-wget wget http://eddylab.org/software/hmmer3/3.1b2/hmmer-3.1b2-linux-intel-x86_64.tar.gz
+wget http://eddylab.org/software/hmmer3/3.1b2/hmmer-3.1b2-linux-intel-x86_64.tar.gz
 tar -xvzf hmmer-3.1b2-linux-intel-x86_64.tar.gz -C $HOME
