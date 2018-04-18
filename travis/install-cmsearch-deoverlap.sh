15a4605a793f401bd62bb29ec5e0ee83063b3f9d
mscheremetjew <maxim@ebi.ac.uk>
2018-04-18 16:39:26 +0100

    Made files executable.

--- travis/install-cmsearch-deoverlap.sh
+++ travis/install-cmsearch-deoverlap.sh
@@ -3,3 +3,4 @@
 travis/install-cwl-runner.sh
 wget https://github.com/nawrockie/cmsearch_tblout_deoverlap/archive/0.02.tar.gz
 tar -xvzf 0.02.tar.gz -C $HOME
+chmod +x -R $HOME/cmsearch_tblout_deoverlap-0.02
