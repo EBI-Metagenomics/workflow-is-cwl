e7d46643881c90a55022f367806f1f87267a91ad
mscheremetjew <maxim@ebi.ac.uk>
2018-05-25 13:27:28 +0100

    Added nodejs install.

--- travis/workflow-cwl-validation-tests.sh
+++ travis/workflow-cwl-validation-tests.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
+export PATH="$HOME/node-v8.11.1:$PATH"
+export PATH="$HOME/miniconda/bin:$PATH"
+source activate cwl-environment
+
 set -e
 
 # run a conformance test for all CWL descriptions
