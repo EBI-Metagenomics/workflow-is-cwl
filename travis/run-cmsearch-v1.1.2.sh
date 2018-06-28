a6a701f3c96adfa3df0ad16fd19e03b2bcbc6fba
mscheremetjew <maxim@ebi.ac.uk>
2018-06-28 15:03:21 +0100

    Added no container option.

--- travis/run-cmsearch-v1.1.2.sh
+++ travis/run-cmsearch-v1.1.2.sh
@@ -6,6 +6,6 @@ export PATH="$HOME/miniconda/bin:$PATH"
 source activate cwl-environment
 
 cd tools/Infernal/cmsearch
-CMD="cwl-runner infernal-cmsearch-v1.1.2.cwl infernal-cmsearch.test.job.yaml"
+CMD="cwl-runner --no-container infernal-cmsearch-v1.1.2.cwl infernal-cmsearch.test.job.yaml"
 echo $CMD
 $CMD
