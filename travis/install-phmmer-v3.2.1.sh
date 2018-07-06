a2d301d9511839528c6e41bb9d9d9a00ffba466d
mscheremetjew <maxim@ebi.ac.uk>
2018-07-06 12:21:52 +0100

    Added mkdir line.

--- travis/install-phmmer-v3.2.1.sh
+++ travis/install-phmmer-v3.2.1.sh
@@ -3,8 +3,9 @@
 APP_VERSION=3.2.1
 travis/install-cwl-runner.sh
 wget http://eddylab.org/software/hmmer/hmmer-$APP_VERSION.tar.gz
-tar -xvzf hmmer-$APP_VERSION.tar.gz -C $HOME/src
-cd $HOME/src/hmmer-$APP_VERSION
+mkdir $HOME/temp
+tar -xvzf hmmer-$APP_VERSION.tar.gz -C $HOME/temp
+cd $HOME/temp/hmmer-$APP_VERSION
 ./configure --prefix $HOME/hmmer-$APP_VERSION
 make -j
 make install
