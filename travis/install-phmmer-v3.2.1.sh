e13270267fe0bf935ca4b90eb6717fb8b69d2704
mscheremetjew <maxim@ebi.ac.uk>
2018-07-06 09:58:05 +0100

    Minor fix.

--- travis/install-phmmer-v3.2.1.sh
+++ travis/install-phmmer-v3.2.1.sh
@@ -4,7 +4,7 @@ APP_VERSION=3.2.1
 travis/install-cwl-runner.sh
 wget http://eddylab.org/software/hmmer/hmmer-$APP_VERSION.tar.gz
 tar -xvzf hmmer-$APP_VERSION.tar.gz -C $HOME
-cd hmmer-$APP_VERSION
+cd $HOME/hmmer-$APP_VERSION
 ./configure
 make -j
 make install
