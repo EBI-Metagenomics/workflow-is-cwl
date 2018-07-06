816f8467e8dc9c844fc0049452cd02df454a45af
mscheremetjew <maxim@ebi.ac.uk>
2018-07-06 11:38:46 +0100

    Changed default installation location.

--- travis/install-phmmer-v3.2.1.sh
+++ travis/install-phmmer-v3.2.1.sh
@@ -3,9 +3,9 @@
 APP_VERSION=3.2.1
 travis/install-cwl-runner.sh
 wget http://eddylab.org/software/hmmer/hmmer-$APP_VERSION.tar.gz
-tar -xvzf hmmer-$APP_VERSION.tar.gz -C $HOME
-cd $HOME/hmmer-$APP_VERSION
-./configure
+tar -xvzf hmmer-$APP_VERSION.tar.gz -C $HOME/src
+cd $HOME/src/hmmer-$APP_VERSION
+./configure --prefix $HOME/hmmer-$APP_VERSION
 make -j
 make install
 cd easel
