05b0f6dd719a9ce6f2e0e7635ca22df53fbd3fa0
mscheremetjew <maxim@ebi.ac.uk>
2018-07-16 14:34:34 +0100

    Added I5 output example file.

--- travis/run-interproscan5.sh
+++ travis/run-interproscan5.sh
@@ -5,7 +5,6 @@ export PATH="$HOME/miniconda/bin:$PATH"
 source activate cwl-environment
 
 cd tools/InterProScan
-touch test_single_protein.fasta.i5_annotations
 CMD="cwl-runner --no-container InterProScan-v5.cwl InterProScan-v5.test.job.yaml"
 echo $CMD
 $CMD
