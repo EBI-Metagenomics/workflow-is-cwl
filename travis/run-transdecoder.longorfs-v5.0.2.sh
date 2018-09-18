08a47df6a55fb2720f013b1c1991caa26e35dcd7
mscheremetjew <maxim@ebi.ac.uk>
2018-09-18 17:13:28 +0100

    Migrated TransDecoder integration tests to docker service.

--- travis/run-transdecoder.longorfs-v5.0.2.sh
+++ travis/run-transdecoder.longorfs-v5.0.2.sh
@@ -1,11 +1,9 @@
 #!/usr/bin/env bash
 
-export PATH="$HOME/TransDecoder-TransDecoder-v5.0.2:$PATH"
 export PATH="$HOME/miniconda/bin:$PATH"
-#echo $PATH
 source activate cwl-environment
 
 cd tools/TransDecoder
-CMD="cwl-runner --no-container TransDecoder.LongOrfs-v5.cwl TransDecoder.LongOrfs-v5.test.job.yaml"
+CMD="cwl-runner TransDecoder.LongOrfs-v5.cwl TransDecoder.LongOrfs-v5.test.job.yaml"
 echo $CMD
 $CMD
