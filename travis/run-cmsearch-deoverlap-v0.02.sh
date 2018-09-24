628dca549ddd0673ff05cd4a96c3e0679c9fe6ea
mscheremetjew <maxim@ebi.ac.uk>
2018-09-24 16:56:18 +0100

    Migrated cmsearch-deoverlap to docker service.

--- travis/run-cmsearch-deoverlap-v0.02.sh
+++ travis/run-cmsearch-deoverlap-v0.02.sh
@@ -1,11 +1,9 @@
 #!/usr/bin/env bash
 
-export PATH="$HOME/cmsearch_tblout_deoverlap-0.02:$PATH"
 export PATH="$HOME/miniconda/bin:$PATH"
-#echo $PATH
 source activate cwl-environment
 
 cd tools/cmsearch-deoverlap
-CMD="cwl-runner --no-container cmsearch-deoverlap-v0.02.cwl cmsearch-deoverlap-v0.02.test.job.yaml"
+CMD="cwl-runner cmsearch-deoverlap-v0.02.cwl cmsearch-deoverlap-v0.02.test.job.yaml"
 echo $CMD
 $CMD
