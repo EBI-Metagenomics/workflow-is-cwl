c9c8f352e19ad7043bd242f6f6df2829b93e0a6f
mscheremetjew <maxim@ebi.ac.uk>
2019-02-05 12:26:15 +0000

    Added tool installs again.

--- travis/install-cmsearch-deoverlap-v0.02.sh
+++ travis/install-cmsearch-deoverlap-v0.02.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-travis/install-cwl-runner.sh
 wget https://github.com/nawrockie/cmsearch_tblout_deoverlap/archive/0.02.tar.gz
 tar -xvzf 0.02.tar.gz -C $HOME
 chmod +x -R $HOME/cmsearch_tblout_deoverlap-0.02
--- travis/install-cmsearch-v1.1.2.sh
+++ travis/install-cmsearch-v1.1.2.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-travis/install-cwl-runner.sh
 wget http://eddylab.org/infernal/infernal-1.1.2.tar.gz
 tar -xvzf infernal-1.1.2.tar.gz -C $HOME
 cd $HOME/infernal-1.1.2
